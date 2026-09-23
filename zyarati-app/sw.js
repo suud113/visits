2b20bf5bf6e12f86143d7ee761235a0375817efd
suud113 <stahtamoni52@gmail.com>
2026-09-23 20:35:12 +0300

    Add files via upload

--- zyarati-app/sw.js
+++ zyarati-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "visits-v12";
+const CACHE = "visits-v13";
 const SHELL = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png", "exceljs.min.js"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
