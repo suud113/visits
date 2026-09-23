e40b939cc68585da127d058e994c568a1bfa5106
suud113 <stahtamoni52@gmail.com>
2026-09-23 17:42:41 +0300

    Add files via upload

--- zyarati-app/sw.js
+++ zyarati-app/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "visits-v2";
-const SHELL = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png"];
+const CACHE = "visits-v3";
+const SHELL = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png", "exceljs.min.js"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
 // network first for the app itself (so updates arrive), cache as offline fallback
